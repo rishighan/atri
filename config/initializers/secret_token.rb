61c3bbe7d5abcf2c6a3dde41157dd4079164447f
Rishi Ghan <rishi.ghan@gmail.com>
2014-07-24 18:28:56 -0400

    test app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Atri::Application.config.secret_key_base = '0ac83e5d5bcdefaed800d8f29cb76bcd05815962e1444d0e42c27eaf05b7e346be59cecc8a025ff78d58717fe9d99449997eaf779ee1efdb95cb4cf53b772c0d'
