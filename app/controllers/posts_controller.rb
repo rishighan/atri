d911d872e3ff96acf03e941a4248ec26d773397d
Rishi Ghan <rishi.ghan@gmail.com>
2014-08-05 18:02:15 -0400

    onto the frontend

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,8 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    #@posts = Post.all
+    render "haiku"
   end
 
   # GET /posts/1
