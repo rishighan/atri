00e2ece0509e836f8455a02a010bc90aef659119
Rishi Ghan <rishi.ghan@gmail.com>
2014-08-07 12:16:55 -0400

    tweaked the select query on categories

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 class HomeController < ApplicationController
-  layout 'site_layout'
+  layout 'site_layout', only: [:index]
 
   def index
     @projects = Post.is_draft("no").group_by_category("include", "Projects")
-    @posts = Post.is_draft("no").group_by_category("include", "General")
+    @posts = Post.is_draft("no").group_by_category("include", "Hero","General")
+    # All posts tagged with everything other than projects will be in the main body
+    @log = Post.is_draft("no").group_by_category("exclude", ["Projects, Hero"])
   end
 
   def haiku
