97c1c2aadb55b76bac66b15d4fd103bb7858111f
Rishi Ghan <rishi.ghan@gmail.com>
2014-08-07 12:26:51 -0400

    fixed an argument error in a query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     @projects = Post.is_draft("no").group_by_category("include", "Projects")
     @posts = Post.is_draft("no").group_by_category("include", "Hero","General")
     # All posts tagged with everything other than projects will be in the main body
-    @log = Post.is_draft("no").group_by_category("exclude", ["Projects, Hero"])
+    @log = Post.is_draft("no").group_by_category("exclude", "Projects, Hero")
   end
 
   def haiku
