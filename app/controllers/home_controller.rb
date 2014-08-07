5531d0482b0cf3f683a0f553854f97ef42cff6dc
Rishi Ghan <rishi.ghan@gmail.com>
2014-08-07 18:38:02 -0400

    adding vestigial changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     #jerry-rigged mechanism to filter our only "Hero" posts that may fall into whatever
     #categories I specify
     @projects = Post.is_draft("no").projects & Post.hero
-    @posts = Post.is_draft("no").group_by_category("include", ["General"]) & Post.hero
+    @posts = Post.is_draft("no").group_by_category("include", ["General", "Technical"]) & Post.hero
     # All posts tagged with everything other than projects will be in the main body
     @log = Post.is_draft("no").group_by_category("exclude", ["Projects", "Hero"])
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ActiveRecord::Base
     has_and_belongs_to_many :categories, join_table: :categories_posts
     accepts_nested_attributes_for :categories
 
-    #scope that isolates "heroes"
+    #scope that isolates "heroes" and "projects"
     scope :hero, -> {Post.includes(:categories).where('categories.title = (?)', "Hero")}
     scope :projects, -> {Post.includes(:categories).where('categories.title = (?)', "Projects")}
 
