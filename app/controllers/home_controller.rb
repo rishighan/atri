d6151a4d77151e8d597fdf9b16fc84056a3dc38d
Rishi Ghan <rishighan@Rishis-Mac-Pro.local>
2014-08-08 08:21:13 -0400

    working on the main body

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
     @projects = Post.is_draft("no").projects & Post.hero
     @posts = Post.is_draft("no").group_by_category("include", ["General", "Technical"]) & Post.hero
     # All posts tagged with everything other than projects will be in the main body
-    @log = Post.is_draft("no").group_by_category("exclude", ["Projects", "Hero"])
+    @log = Post.is_draft("no").group_by_category("exclude", ["Projects", "Hero", "Feature"])
   end
 
   def haiku
