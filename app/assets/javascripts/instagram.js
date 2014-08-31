e1655a4e04ee9bab70b0c67074a43c311c9f4522
Rishi Ghan <rishi.ghan@gmail.com>
2014-08-30 20:01:11 -0400

    instagram js

--- app/assets/javascripts/instagram.js
+++ app/assets/javascripts/instagram.js
@@ -3,6 +3,8 @@
 // ninth muse and the usuals
 
 var jiff = function(){
+ var clientid = "9f748070d00748cf8c845f874800ab00",
+     clientsecret = "436bcc784d874a9bb3a450eb138b3193";
 
 }
 
@@ -15,9 +17,16 @@ jiff.prototype = {
     request.onreadystatechange = function(){
       if(request.readyState == 4 && request.status == 200){
         callback(request.responseText)
+
       }
     }
     request.send(null);
+  },
+
+  spill: function(response){
+
+    console.log(response);
+
   }
 
 }
