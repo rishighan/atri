09fbf34f656f823aceb8c0a3dafed7c821d3f3f8
Rishi Ghan <rishi.ghan@gmail.com>
2014-12-14 14:17:25 -0500

    different layout for admin index page

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -21,47 +21,44 @@ function Heroize(opts) {
 
     //set the options
     this.heroHeight = opts.heroHeight || 510,
-        this.heroWidth = opts.heroWidth || 100,
-        this.margin = opts.margin || "0 0 -510px, 0",
-        this.bgSize = opts.bgSize || "cover",
-        this.position = opts.position || "center center",
-        this.repeat = opts.repeat || "no-repeat",
-        this.bgAttachment = opts.bgAttachment || "fixed",
-        this.overflow = opts.overflow || "hidden",
+    this.heroWidth = opts.heroWidth || 100,
+    this.margin = opts.margin || "0 0 -510px, 0",
+    this.bgSize = opts.bgSize || "cover",
+    this.position = opts.position || "center center",
+    this.repeat = opts.repeat || "no-repeat",
+    this.bgAttachment = opts.bgAttachment || "fixed",
+    this.overflow = opts.overflow || "hidden",
 
-        // target div, image element and hero bg container
-        this.heroBgContainer = opts.container || 'project-hero-bg',
-        this.imageSrc = opts.imageSrc || 'data-src',
-        this.heroBgContainer = opts.heroBgContainer || 'hero-bg',
+    // target div, image element and hero bg container
+    this.heroBgContainer = opts.container || 'project-hero-bg',
+    this.imageSrc = opts.imageSrc || 'data-src',
+    this.heroBgContainer = opts.heroBgContainer || 'hero-bg',
 
-        // target for color-thief
-        this.colorTarget = opts.colorTarget || 'color-target';
-
-
+    // target for color-thief
+    this.colorTarget = opts.colorTarget || 'color-target';
 }
 
 
 Heroize.prototype = {
-
     setProjectHeroImage: function() {
 
-        // hero image
-        var projectHeroBg = this.heroBgContainer,
+    // hero image
+    var projectHeroBg = this.heroBgContainer,
 
-            // get url
-            imgsrc = projectHeroBg.getAttribute(this.imgSrc),
-            heroBgContainer = document.getElementById(this.heroBgContainer); //container
+    // get url
+    imgsrc = projectHeroBg.getAttribute(this.imgSrc),
+    heroBgContainer = document.getElementById(this.heroBgContainer); //container
 
-        // change style
-        heroBgContainer.style.backgroundSize = this.bgSize;
-        heroBgContainer.style.backgroundImage = "url(" + this.imageSrc + ")";
-        heroBgContainer.style.margin = this.margin;
-        heroBgContainer.style.height = this.heroHeight + "px";
-        heroBgContainer.style.width = this.heroWidth + "%";
-        heroBgContainer.style.overflow = this.overflow;
-        heroBgContainer.style.backgroundPosition = this.position;
-        heroBgContainer.style.backgroundAttachment = this.bgAttachment;
-        heroBgContainer.style.backgroundRepeat = this.repeat;
+    // change style
+    heroBgContainer.style.backgroundSize = this.bgSize;
+    heroBgContainer.style.backgroundImage = "url(" + this.imageSrc + ")";
+    heroBgContainer.style.margin = this.margin;
+    heroBgContainer.style.height = this.heroHeight + "px";
+    heroBgContainer.style.width = this.heroWidth + "%";
+    heroBgContainer.style.overflow = this.overflow;
+    heroBgContainer.style.backgroundPosition = this.position;
+    heroBgContainer.style.backgroundAttachment = this.bgAttachment;
+    heroBgContainer.style.backgroundRepeat = this.repeat;
 
 
         //console.log(projectHeroBg)
@@ -100,9 +97,12 @@ $(document).ready(function() {
 
     // set the options object
     var opts = {
-
-    }
-
+      colorTarget: '#color-target'
+    },
+    foo = new Heroize(opts);
+    foo.setProjectHeroImage();
+    foo.setDominantColor(opts.colorTarget)
+    console.log(opts.colorTarget)
     setProjectHeroImage();
     setDominantColor('#color-target');
 
