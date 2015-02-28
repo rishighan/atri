beebdaf77aee68d8872c6e22db36e76b48155576
Rishi Ghan <rishi.ghan@gmail.com>
2015-02-28 18:51:41 -0500

    removed yet again

--- app/models/pageviews.rb
+++ app/models/pageviews.rb
@@ -1,24 +1,24 @@
-#reporting model
-class Pageviews
-  extend Legato::Model
+# #reporting model
+# class Pageviews
+#   extend Legato::Model
 
-  metrics :pageviews, :uniquePageviews
-  dimensions :pagePathLevel1
+#   metrics :pageviews, :uniquePageviews
+#   dimensions :pagePathLevel1
 
-  filter(:by_index_in_path_level_1) {|page_path_level1| contains(:pagePathLevel1, 'index')}
+#   filter(:by_index_in_path_level_1) {|page_path_level1| contains(:pagePathLevel1, 'index')}
 
-  def self.query(profile, start_date, end_date)
-    Pageviews.results(profile,
-                      :start_date => start_date,
-                      :end_date => end_date
-    )
-    # Just for reference, sorting descending by pageviews is done by:   :sort => '-pageviews'
-  end
+#   def self.query(profile, start_date, end_date)
+#     Pageviews.results(profile,
+#                       :start_date => start_date,
+#                       :end_date => end_date
+#     )
+#     # Just for reference, sorting descending by pageviews is done by:   :sort => '-pageviews'
+#   end
 
-  def self.query_index(profile, start_date, end_date)
-    Pageviews.by_index_in_path_level_1.results(profile,
-                                               :start_date => start_date,
-                                               :end_date => end_date
-    )
-  end
-end
+#   def self.query_index(profile, start_date, end_date)
+#     Pageviews.by_index_in_path_level_1.results(profile,
+#                                                :start_date => start_date,
+#                                                :end_date => end_date
+#     )
+#   end
+# end
