b3b04d496b04740a3dc81771babcb02d9d1e8f24
Rishi Ghan <rishi.ghan@gmail.com>
2015-03-15 14:10:14 -0400

    removed a comment from the sparkline container that was messing up the line graph

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,5 @@
-$(document).ready(function(){
-    $('.sparky').sparkline('html', {type:'line'});
+$(document).ready(function() {
+    $('.sparky').sparkline("html",{type:"line"});
 })
 
-$(document).on('page:load', function() {
-    $('.sparky').sparkline('html', {type:'line'});
-});
+
--- app/models/pageviews.rb
+++ app/models/pageviews.rb
@@ -1,28 +1,11 @@
 class Pageviews < ActiveRecord::Base
-#google analytics api shenanigans
-require 'google/api_client'
-require 'date'
+  #google analytics api shenanigans
+  require 'google/api_client'
+  require 'date'
+  include ReportingHelper
 
-  SERVICE_ACCOUNT_EMAIL = '142715827895-7j2bdg768lnqkgdmfat8r47fen5qlcpr@developer.gserviceaccount.com'
-  KEY_PATH = File.join(Rails.root, '/config/', 'rishighan-e2d48dda2835.p12')
-  PROFILE = 'ga:17894417'
-
-  def self.result post
-    client = Google::APIClient.new(:application_name => 'rishighan.com', :application_version => '1')
-
-    key = Google::APIClient::PKCS12.load_key(KEY_PATH, 'notasecret')
-    service_account = Google::APIClient::JWTAsserter.new(
-      SERVICE_ACCOUNT_EMAIL,
-      ['https://www.googleapis.com/auth/analytics.readonly', 'https://www.googleapis.com/auth/prediction'],
-    key)
-    client.authorization = service_account.authorize
-    analytics = client.discovered_api('analytics', 'v3')
-
-    parameters = {
-      'accountId' => '~all',
-      'webPropertyId' => '~all'
-    }
-
+  def self.getviews post
+  	client, analytics, parameters = ReportingHelper.initclient
     result   = client.execute(:api_method => analytics.management.profiles.list, parameters: parameters)
     profiles = result.data.items
 
@@ -35,7 +18,7 @@ require 'date'
       'filters'     => "ga:pagePath=~/#{post}"
     }
     result = client.execute(:api_method => analytics.data.ga.get, :parameters => parameters)
-    interim = result.data.rows.map{|hit| p hit[1]}
+    interim = result.data.rows.map{|hit| hit[1].to_i}.join(', ')
 
   end
 
