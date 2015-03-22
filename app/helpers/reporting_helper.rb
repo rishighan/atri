f2aaffd8ce9f59372e6b8c02fcd0ff356fe955f9
Rishi Ghan <rishi.ghan@gmail.com>
2015-03-22 19:16:50 -0400

    adding trampoline layout

--- app/helpers/reporting_helper.rb
+++ app/helpers/reporting_helper.rb
@@ -5,17 +5,19 @@ module ReportingHelper
   SERVICE_ACCOUNT_EMAIL = '142715827895-7j2bdg768lnqkgdmfat8r47fen5qlcpr@developer.gserviceaccount.com'
   KEY_PATH = File.join(Rails.root, '/config/', 'rishighan-e2d48dda2835.p12')
   PROFILE = 'ga:17894417'
+  API_VERSION = 'v3'
 
   def self.initclient
     client = Google::APIClient.new(:application_name => 'rishighan.com', :application_version => '1')
 
     key = Google::APIClient::PKCS12.load_key(KEY_PATH, 'notasecret')
     service_account = Google::APIClient::JWTAsserter.new(
-      				  SERVICE_ACCOUNT_EMAIL,
-      				  ['https://www.googleapis.com/auth/analytics.readonly', 'https://www.googleapis.com/auth/prediction'],
-    				  key)
+      SERVICE_ACCOUNT_EMAIL,
+      ['https://www.googleapis.com/auth/analytics.readonly', 'https://www.googleapis.com/auth/prediction'],
+    key)
     client.authorization = service_account.authorize
-    analytics = client.discovered_api('analytics', 'v3')
+    analytics = client.discovered_api('analytics', API_VERSION)
+
 
     parameters = {
       'accountId' => '~all',
--- app/models/pageviews.rb
+++ app/models/pageviews.rb
@@ -20,11 +20,11 @@ class Pageviews < ActiveRecord::Base
     result.data.rows.map{|hit| hit[1].to_i}.join(', ')
 
     #profiles = result.data.items
-    # interim = Rails.cache.fetch('google_analytics_api/#{cache_key}', expires_in: 2.minutes) do
+    # cache_key = post
+    # Rails.cache.fetch('google_analytics_api/#{cache_key}', expires_in: 2.minutes) do
     #   result = client.execute(:api_method => analytics.data.ga.get, :parameters => parameters)
     #   result.data.rows.map{|hit| hit[1].to_i}.join(', ')
+
     # end
-
   end
-
 end
