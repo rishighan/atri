8fbaca3f7da2cd12c3cca022a11ae4c9b72133eb
Rishi Ghan <rishi.ghan@gmail.com>
2015-03-22 20:20:23 -0400

    added notice to the nav

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
-    $('.sparky').sparkline("html",{type:"line"});
+    $('.sparky').sparkline("html", {
+        type: "line"
+    });
+
+    // for notices
+    setTimeout(function() {
+        $('.sitewide-notice').fadeOut('slow');
+    }, 1000);
 })
 
 $(document).on('page:load', function() {
-    $('.sparky').sparkline("html",{type:"line"});
+    $('.sparky').sparkline("html", {
+        type: "line"
+    });
 })
